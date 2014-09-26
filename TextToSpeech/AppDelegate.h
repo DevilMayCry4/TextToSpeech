5a3ddd79d331f229c2403b57cd5644464a196e88
brettdidonato <brett.didonato@gmail.com>
2014-09-26 12:57:34 -0400

    Initial Commit

---
+++ TextToSpeech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextToSpeech
+//
+//  Created by Brett DiDonato on 9/26/14.
+//  Copyright (c) 2014 BSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextToSpeech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextToSpeech
+//
+//  Created by Brett DiDonato on 9/26/14.
+//  Copyright (c) 2014 BSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
